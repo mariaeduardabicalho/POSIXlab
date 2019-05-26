af6dab5fe656e32b98f72997b3f1d39c3eebec64
mariaeduardabicalho <mariaeduardabicalho@gmail.com>
2019-05-26 17:43:35 -0300

    B+0,25 alcancado

---
+++ lab-posix/src/mintest/runner.h
@@ -0,0 +1,73 @@
+int main() {
+  
+    int size = sizeof(all_tests)/sizeof(test_data);
+    printf("Running %d tests:\n", size);
+    printf("=====================\n\n");
+    int pass_count = 0;
+
+    // Inicializações para forks e o waitpid()
+    pid_t f[size];  
+    pid_t w[size];
+    int wstatus;
+
+    // Inicializações para pipes
+
+    char bufs;
+    int fds[size];
+
+
+    
+
+
+    // criacao dos forks e wait
+
+    for (int i = 0; i < size; i++) {
+        fds[i]= open("/tmp", O_TMPFILE | O_RDWR, 0600);
+        
+        f[i]= fork();
+        printf("\033[0m");
+        
+        
+        if(f[i] == 0){
+            alarm(1);
+            dup2(fds[i],1);
+            if (all_tests[i].function() >= 0) {
+                printf("\033[0;32m %s:  [PASS]\n", all_tests[i].name);
+                return 0;
+            }; 
+            return -1;
+        }
+        
+    }
+
+    for (int i = 0; i < size; i++) {
+        
+        
+        w[i]=  waitpid(f[i],&wstatus,NULL);  
+        
+        if (WIFEXITED(wstatus)){
+            printf("\033[0m %s exited, status= %d\n",all_tests[i].name, WEXITSTATUS(wstatus)); 
+            if(WEXITSTATUS(wstatus) == 0){
+                pass_count++;
+            }
+            
+        } 
+        if(WIFSIGNALED(wstatus)){
+            printf("\033[0m %s : \033[0;31m [FAIL] com sinal: %s\n \n ",all_tests[i].name,strsignal(WTERMSIG(wstatus)));
+        }
+         
+       
+        lseek(fds[i],0,SEEK_SET);
+        
+        while(read(fds[i],&bufs,1)>0){
+    
+            printf("%c",bufs);
+
+        }
+        //printf("\033[0m Tempo decorrido: %d\n",);
+    }
+
+    printf("\033[0m \n\n=====================\n");
+    printf("%d/%d tests passed\n", pass_count, size);
+    return 0;
+}
---
+++ lab-posix/src/testes.c
@@ -0,0 +1,57 @@
+#include "mintest/macros.h"
+
+int test1() {
+    //test_printf("segmentation fault\n");
+    int *p = NULL;
+    *p = 1;
+    // nem preciso de trst assert ??
+    return 0;
+}
+
+int test2() {
+    //printf("divissao por zero\n");
+    int i = 10/0 ;
+    return 0;
+    // nem preciso de trst assert ??
+}
+
+int test3() {
+   // test_printf("loop infinito");
+    while(1){
+
+    }
+    return 0;
+    // nem preciso de trst assert ??
+}
+int test46() {
+    //tipo teste  4 e 6
+    int t;
+    for(int i=0; i<20; i++){
+        t+=i;
+        printf("%d\n",i);
+    }
+    return 0;
+    // isso seria muito trabalho?
+}
+int test5() {
+ // por exemplo isso?     
+    test_assert(1 == 1, "This always succeeds");
+    test_assert(0 == 1, "This always fails");
+
+    return 0;
+}
+
+int test71() {     
+    sleep(1000);
+
+    return 0;
+}
+int test72() {    
+    return 0;
+}
+
+
+
+test_list = { TEST(test1), TEST(test2), TEST(test3), TEST(test46),TEST(test5),TEST(test71),TEST(test72)};
+
+#include "mintest/runner.h"
